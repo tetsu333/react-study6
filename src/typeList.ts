070ebf97baa6ef92b4918cfef7918302ec723e1f
tetsu333 <70054989+tetsu333@users.noreply.github.com>
2022-06-01 02:05:48 +0900

    TypeScript基礎

---
+++ src/typeList.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+let bool: boolean = true;
+
+let num: number = 0;
+
+let str: string = "A";
+
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+let tuple: [number, string] = [0, "A"];
+
+let any1: any = false;
+
+const funcA = (): void => {
+  const test = "TEST";
+};
+
+let null1: null = null;
+
+let undefined1: undefined = undefined;
+
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "AA" };
